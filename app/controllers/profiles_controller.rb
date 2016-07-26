0bc8b1750583e530ba31ef298ca866ab30f85212
Rachel Mahan <rachelm2490@gmail.com>
2016-07-26 18:04:41 -0400

    Purravida, bootstrap fixes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,10 @@
 class ProfilesController < ApplicationController
+
+	def sitter_settings
+		render :sitter_settings
+	end
+
+	def home_settings
+		render :home_settings
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   
   
   get '/' => 'static#index'
-  
+  get '/sitter_settings' => 'profiles#sitter_settings'
+  get '/home_settings' => 'profiles#home_settings'
   devise_for :users
   devise_for :views
   
