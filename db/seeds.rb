e45e6824623338cec876c8b89d91521c7a89e97c
Rachel Mahan <rachelm2490@gmail.com>
2016-08-03 20:58:11 -0400

    Purravida, launch 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,5 +13,3 @@ rachel, liz, leah = User.create([
 	{name: "leah", email:"leah@shopplanetblue.com" , password: "password", password_confirmation:"password"}
 	])
 
-mindy = User.create( name:"mindy jones", email: "mindy@morkandmindy.com" , password:"password", password_confirmation:"password",
-		is_active_sitter: true, about_me: "used to be on TV")
