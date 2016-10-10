c6458726910d32189ffd7b16d133e2347e65b572
Rachel Mahan <rachelm2490@gmail.com>
2016-10-09 22:24:02 -0400

    section 4 re-styling, add hover effect

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$( document ).ready(function() {
+    $("[rel='tooltip']").tooltip();    
+ 
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    ); 
+});
